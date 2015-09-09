94eed4d64bec7ee0dccb0b7180abff61c032ce4c
Stardog <mcnaney@gmail.com>
2015-09-09 15:18:53 -0400

    Eligibility and deletion methods added.
    - Students ordered by last name by default.
    - Banning a student sets eligibility reason to "banned"
    - Added methods to make a student ineligible and eligible.
    - Added method to delete a student.

--- class/Factory/Student.php
+++ class/Factory/Student.php
@@ -37,18 +37,21 @@ class Student extends Base
         self::saveResource($student);
     }
 
-    public function getList($mode = TG_LIST_ALL, $order_by = null)
+    public function getList($mode = TG_LIST_ALL, $order_by = null, $order_dir = 'asc')
     {
-        $db = $this->getListDB($mode, $order_by);
+        if (empty($order_by)) {
+            $order_by = 'last_name';
+        }
+        $db = $this->getListDB($mode, $order_by, $order_dir);
         $tbl = $db->getTable($this->table);
-        
+
         $limit = filter_input(INPUT_GET, 'limit', FILTER_SANITIZE_NUMBER_INT);
         if (empty($limit)) {
             $limit = 50;
         }
-        
+
         $search = filter_input(INPUT_GET, 'search', FILTER_SANITIZE_STRING);
-        
+
         if (!empty($search)) {
             $c1 = $db->createConditional($tbl->getField('first_name'), "%$search%", 'like');
             $c2 = $db->createConditional($tbl->getField('last_name'), "%$search%", 'like');
@@ -57,19 +60,18 @@ class Student extends Base
             $c5 = $db->createConditional($c3, $c4, 'or');
             $db->addConditional($c5);
         }
-        
         $db->setLimit($limit);
-        //echo $db->selectQuery();exit;
         $result = $db->select();
         return $result;
     }
-    
+
     public function ban($student_id, $reason)
     {
         $student = new Resource;
         self::loadByID($student, $student_id);
         $student->setBanned(true);
-        $student->setBannedReason($reason);
+        $student->setBannedReason(filter_var($reason, FILTER_SANITIZE_STRING));
+        $student->setIneligibleReason('Banned');
         $student->stampBanned();
         $student->setEligible(false);
         self::saveResource($student);
@@ -85,5 +87,31 @@ class Student extends Base
         $student->setEligible(true);
         self::saveResource($student);
     }
+
+    public function ineligible($student_id, $reason)
+    {
+        $student = new Resource;
+        self::loadByID($student, $student_id);
+        $student->setEligible(false);
+        $student->setIneligibleReason(filter_var($reason, FILTER_SANITIZE_STRING));
+        self::saveResource($student);
+    }
+
+    public function eligible($student_id)
+    {
+        $student = new Resource;
+        self::loadByID($student, $student_id);
+        $student->setEligible(true);
+        $student->setIneligibleReason('');
+        self::saveResource($student);
+    }
+    
+    public function delete($student_id)
+    {
+        $student = new Resource;
+        $student->setId($student_id);
+        self::deleteResource($student);
+    }
+            
 
 }
