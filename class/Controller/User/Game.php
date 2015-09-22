1cc4c125d9b3bd1cca0594d2a0226c110a233522
Stardog <mcnaney@gmail.com>
2015-09-22 12:36:19 -0400

    Lottery functions moved into their own controller.

--- class/Controller/User/Game.php
+++ class/Controller/User/Game.php
@@ -21,10 +21,6 @@ class Game extends Base
             case 'getCurrent':
                 $json = $this->getCurrent();
                 break;
-
-            case 'getLottery':
-                $json = $this->getLottery();
-                break;
         }
 
         $view = new \View\JsonView($json);
@@ -35,44 +31,11 @@ class Game extends Base
     {
         $factory = new Factory;
         $game = $factory->getCurrent();
-        return $game->getStringVars();
-    }
-
-    private function getLottery()
-    {
-        $studentFactory = new \tailgate\Factory\Student;
-        $student = $studentFactory->getCurrentStudent();
-        $factory = new \tailgate\Factory\Lottery;
-        $game_id = filter_input(INPUT_GET, 'game_id', FILTER_SANITIZE_NUMBER_INT);
-        $student_id = $student->getId();
-        $lottery = $factory->getLotteryEntry($game_id, $student_id);
-        if ($lottery) {
-            return $lottery->getStringVars();
+        if ($game) {
+            return $game->getStringVars();
         } else {
             return null;
         }
-    }
-
-    public function post(\Request $request)
-    {
-        
-        $view = new \View\JsonView(array('success' => true));
-        $response = new \Response($view);
-
-        $command = $request->getVar('command');
-        switch ($command) {
-            case 'apply':
-                $this->applyToLottery();
-                break;
-        }
-        return $response;
-    }
-
-    private function applyToLottery()
-    {
-        $lottery = new \tailgate\Factory\Lottery;
-        $game_id = filter_input(INPUT_POST, 'game_id', FILTER_SANITIZE_NUMBER_INT);
-        $lottery->apply($game_id);
     }
 
 }
