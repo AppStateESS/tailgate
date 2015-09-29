1b3ba2116e5f3759c3b68db16e2b69a5bd3e8ec3
Stardog <mcnaney@gmail.com>
2015-09-29 12:22:49 -0400

    Spot list now contains current lottery information to allow pickup tracking.

--- class/Factory/Spot.php
+++ class/Factory/Spot.php
@@ -16,7 +16,32 @@ class Spot extends Base
     {
         $db = $this->getListDB($mode, 'number');
         $lot_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-        $db->getTable('tg_spot')->addFieldConditional('lot_id', $lot_id);
+        $spotTable = $db->getTable('tg_spot');
+        $spotTable->addFieldConditional('lot_id', $lot_id);
+
+        $gameFactory = new \tailgate\Factory\Game;
+        $current_game = $gameFactory->getCurrent();
+        if ($current_game && $current_game->getLotteryRun()) {
+            $lotteryTable = $db->addTable('tg_lottery');
+            $lotteryTable->addField('picked_up');
+            $lotteryTable->addField('student_id');
+            $lotteryTable->addField('spot_id');
+            $lotteryTable->addField('id', 'lottery_id');
+            $c1 = new \Database\Conditional($db, $lotteryTable->getField('game_id'), $current_game->getId(), '=');
+            $c2 = new \Database\Conditional($db, $lotteryTable->getField('game_id'), null, 'is');
+            $c3 = new \Database\Conditional($db, $c1, $c2, 'or');
+
+            $db->addConditional($c3);
+
+            $studentTable = $db->addTable('tg_student');
+            $studentTable->addField('first_name');
+            $studentTable->addField('last_name');
+
+            $joinCond1 = $db->createConditional($spotTable->getField('id'), $lotteryTable->getField('spot_id'), '=');
+            $joinCond2 = $db->createConditional($lotteryTable->getField('student_id'), $studentTable->getField('id'), '=');
+            $db->joinResources($spotTable, $lotteryTable, $joinCond1, 'left');
+            $db->joinResources($lotteryTable, $studentTable, $joinCond2, 'left');
+        }
         $result = $db->select();
         return $result;
     }
