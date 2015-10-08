075cd1a941c8cc9509e05a24a1cd36643ea74cd0
Stardog <mcnaney@gmail.com>
2015-10-08 11:00:52 -0400

    Added control for admin to force student into spot. Incomplete.

--- class/Controller/Admin/Student.php
+++ class/Controller/Admin/Student.php
@@ -20,6 +20,10 @@ class Student extends Base
             case 'list':
                 $json = $factory->getList();
                 break;
+
+            case 'getAvailableSpots':
+                $json = \tailgate\Factory\Lottery::getAvailableSpots(null, null, true);
+                break;
         }
 
         $view = new \View\JsonView($json);
@@ -30,7 +34,6 @@ class Student extends Base
     {
         $factory = new Factory;
         $view = new \View\JsonView(array('success' => true));
-        $response = new \Response($view);
 
         if (!$request->isVar('command')) {
             throw new \Exception('Bad command');
@@ -43,23 +46,41 @@ class Student extends Base
             case 'unban':
                 $factory->unban($request->getVar('id'));
                 break;
-            
+
             case 'ineligible':
                 $factory->ineligible($request->getVar('id'), $request->getVar('reason'));
                 break;
-            
+
             case 'eligible':
                 $factory->eligible($request->getVar('id'));
                 break;
-            
+
             case 'delete':
                 $factory->delete($request->getVar('id'));
                 break;
-            
+
+            case 'assign':
+                $view = $this->assign();
+                break;
+
             default:
                 throw new \Exception('Bad command:' . $request->getVar('command'));
         }
+        $response = new \Response($view);
         return $response;
+    }
+
+    private function assign()
+    {
+        $spot_id = filter_input(INPUT_POST, 'spotId', FILTER_SANITIZE_NUMBER_INT);
+        $student_id = filter_input(INPUT_POST, 'studentId', FILTER_SANITIZE_NUMBER_INT);
+        if (\tailgate\Factory\Lottery::spotTaken($spot_id)) {
+            $view = new \View\JsonView(array('success' => false));
+        } else {
+            \tailgate\Factory\Lottery::assignStudent($student_id, $spot_id);
+            $view = new \View\JsonView(array('success' => true));
+        }
+        return $view;
     }
 
 }
