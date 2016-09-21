f3e88cb92cfcb377d2149919e68419299ce8cbae
Matthew McNaney <mcnaney@gmail.com>
2016-09-21 13:13:50 -0400

    Input update fix. Spaces added.

--- javascript/Admin/VisitorForm.jsx
+++ javascript/Admin/VisitorForm.jsx
@@ -7,12 +7,23 @@ import Alert from '../Mixin/Alert.jsx'
 class VisitorForm extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {message: ''}
+    this.state = {message: '', mascot: '', university: ''}
+    this.updateMascot = this.updateMascot.bind(this)
+    this.updateUniversity = this.updateUniversity.bind(this)
+    this.save = this.save.bind(this)
+  }
+
+  updateMascot(e) {
+    this.setState({mascot: e.target.value})
+  }
+
+  updateUniversity(e) {
+    this.setState({university: e.target.value})
   }
 
   save() {
-    let university = $('#university').val()
-    let mascot = $('#mascot').val()
+    const university = this.state.university
+    const mascot = this.state.mascot
 
     if (university.length > 0 && mascot.length > 0) {
       $.post('tailgate/Admin/Visitor', {
@@ -36,17 +47,18 @@ class VisitorForm extends React.Component {
         <div className="row well">
           {this.state.message}
           <div className="form-group col-sm-6">
-            <TextInput label={'University:'} inputId={'university'}/>
+            <TextInput label={'University:'} inputId={'university'}
+            value={this.state.university} handleChange={this.updateUniversity}/>
           </div>
           <div className="form-group col-sm-6">
-            <TextInput label={'Mascot:'} inputId={'mascot'}/>
+            <TextInput label={'Mascot:'} inputId={'mascot'} value={this.state.mascot} handleChange={this.updateMascot}/>
           </div>
           <div className="col-sm-12 text-center">
             <button className="btn btn-primary" onClick={this.save}>
-              <i className="fa fa-save"></i>
+              <i className="fa fa-save"></i>&nbsp;
             Save</button>&nbsp;
             <button className="btn btn-default" onClick={this.props.closeForm}>
-              <i className="fa fa-times"></i>
+              <i className="fa fa-times"></i>&nbsp;
             Close</button>
           </div>
         </div>
