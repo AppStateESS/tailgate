ceb5f893d7d940d9b468692c4caf7aa1d8a23dad
Matthew McNaney <mcnaney@gmail.com>
2018-08-27 08:42:10 -0400

    Update onChangeDate Time.
    + onChangeDateTime was sending an incompatible date format
      to the server.
    + eslint updates

--- javascript/Mixin/datepicker.js
+++ javascript/Mixin/datepicker.js
@@ -2,41 +2,42 @@
 
 class Datepicker {
   constructor(update, error, dom, command) {
-    this.update = update;
-    this.error = error;
-    this.dom = dom;
-    this.command = command;
-    this.popupCalendar = this.popupCalendar.bind(this);
+    this.update = update
+    this.error = error
+    this.dom = dom
+    this.command = command
+    this.popupCalendar = this.popupCalendar.bind(this)
   }
 
   dateInit(useTime) {
-    let dateFormat = useTime ? 'Y/m/d H:i' : 'Y/m/d';
+    let dateFormat = useTime
+      ? 'Y/m/d H:i'
+      : 'Y/m/d'
     $(this.dom).datetimepicker({
       timepicker: useTime,
       format: dateFormat,
-      onChangeDateTime: function (ct, i) {
-        this.updateDate(ct);
-      }.bind(this),
-    });
+      onChangeDateTime: function (ct, input) {
+        this.updateDate(input[0].value)
+      }.bind(this)
+    })
   }
 
   popupCalendar() {
-    $(this.dom).datetimepicker('show');
+    $(this.dom).datetimepicker('show')
   }
 
   updateDate(date) {
     $.post('tailgate/Admin/Game', {
-        command: this.command,
-        date: date
-      }, null, 'json')
-      .done(function (data) {
-        if (data.success) {
-          this.update();
-        } else {
-          this.error(data.error);
-        }
-      }.bind(this));
+      command: this.command,
+      date: date,
+    }, null, 'json').done(function (data) {
+      if (data.success) {
+        this.update()
+      } else {
+        this.error(data.error)
+      }
+    }.bind(this))
   }
 }
 
-export default Datepicker;
+export default Datepicker
