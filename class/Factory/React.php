7d50121b8c035df23d97884b2dca182fbc3addea
Matthew McNaney <mcnaney@gmail.com>
2018-08-28 12:49:52 -0400

    Fixed the build path.

--- class/Factory/React.php
+++ class/Factory/React.php
@@ -13,27 +13,47 @@ class React
     {
         static $vendorUsed = false;
         $vendor = null;
-        
+
         $root_directory = PHPWS_SOURCE_HTTP . 'mod/tailgate/javascript';
         if ($development) {
             $subdir = 'dev';
             $script_file = "dev/{$filename}.js";
         } else {
             $subdir = 'build';
-            $script_file = "build/{$filename}.js";
+            $script_file = 'build/' . self::getAssetPath($filename);
         }
-        
+
         if (!$vendorUsed) {
             $vendorUsed = true;
+            if ($development) {
+                $vendorFile = $root_directory . '/dev/vendor.js';
+            } else {
+                $vendorFile = $root_directory . '/build/' . self::getAssetPath('vendor');
+            }
             $vendor = <<<EOF
-<script type="text/javascript" src="$root_directory/$subdir/vendor.js"></script>
+<script type="text/javascript" src="$vendorFile"></script>
 EOF;
         }
         $script_header = <<<EOF
 $vendor
-<script type="text/javascript" src="$root_directory/$subdir/{$filename}.js"></script>
+<script type="text/javascript" src="$root_directory/$script_file"></script>
 EOF;
         return $script_header;
+    }
+
+    protected static function getAssetPath($scriptName)
+    {
+        $rootDirectory = PHPWS_SOURCE_DIR . 'mod/tailgate/';
+        echo $rootDirectory . 'assets.json';
+        if (!is_file($rootDirectory . 'assets.json')) {
+            exit('Missing assets.json file. Run `npm run build` in mod/tailgate directory.');
+        }
+        $jsonRaw = file_get_contents($rootDirectory . 'assets.json');
+        $json = json_decode($jsonRaw, true);
+        if (!isset($json[$scriptName]['js'])) {
+            throw new \Exception('Script file not found among assets.');
+        }
+        return $json[$scriptName]['js'];
     }
 
 }
