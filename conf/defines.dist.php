a59d5365adbdb05918f1ab73979a88f5d7e5549d
Matt McNaney <mcnaneym@appstate.edu>
2022-08-26 10:41:08 -0400

    Adds default file paths needed for PDF.

--- conf/defines.dist.php
+++ conf/defines.dist.php
@@ -4,7 +4,6 @@
  * @license http://opensource.org/licenses/lgpl-3.0.html
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
-
 // Show all items from table
 define('TG_LIST_ALL', 1);
 
@@ -14,7 +13,6 @@ define('TG_LIST_ACTIVE', 2);
 // Show only inactive elements
 define('TG_LIST_INACTIVE', 3);
 
-
 define('TAILGATE_DATE_FORMAT', '%h %e, %Y');
 define('TAILGATE_TIME_FORMAT', '%l:%M%p');
 
@@ -22,6 +20,13 @@ if (!defined('SWIFT_MAIL_TRANSPORT_TYPE')) {
     define('SWIFT_MAIL_TRANSPORT_TYPE', 3);
 }
 
-define('REACT_DEVMODE', false);
+if (!defined('SWIFT_MAIL_TRANSPORT_PARAMETER')) {
+    define('SWIFT_MAIL_TRANSPORT_PARAMETER', '/usr/sbin/sendmail -t -i');
+}
 
+define('REACT_DEVMODE', false);
+define('WKPDF_PATH', '/usr/bin/wkhtmltopdf');
+
+define('USE_XVFB', true);
+define('XVFB_PATH', '/usr/bin/xvfb-run');
 define('TAILGATE_BANNER_URL', 'url/to/banner/site');
