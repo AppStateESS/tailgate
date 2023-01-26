16c3b65f74cb02dd7ea2fee5bf1931fcd9c02f70
Matt McNaney <mcnaneym@appstate.edu>
2023-01-26 11:13:36 -0500

    Updates formatting.

--- javascript/User/ConfirmSpot.jsx
+++ javascript/User/ConfirmSpot.jsx
@@ -7,80 +7,110 @@ import PropTypes from 'prop-types'
 class ConfirmSpot extends React.Component {
   constructor() {
     super()
-    this.state = {availableSpots: [], message: null, waiting: 0, spotChoice: '0'}
+    this.state = {
+      availableSpots: [],
+      message: null,
+      waiting: 0,
+      spotChoice: '0',
+    }
     this.updateSpot = this.updateSpot.bind(this)
     this.confirmSpot = this.confirmSpot.bind(this)
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.loadAvailableSpots()
   }
 
-  loadAvailableSpots () {
-    $.getJSON('tailgate/User/Lottery', {command: 'spotChoice'}).done(function (data) {
-      this.setState({availableSpots: data})
-    }.bind(this))
+  loadAvailableSpots() {
+    $.getJSON('tailgate/User/Lottery', {command: 'spotChoice'}).done(
+      function (data) {
+        this.setState({availableSpots: data})
+      }.bind(this)
+    )
   }
 
-  confirmSpot () {
-    var spotId = this.state.spotChoice
+  confirmSpot() {
+    const spotId = this.state.spotChoice
     if (spotId === '0') {
       return
     }
-    var lotteryId = this.props.lottery.id
+    const lotteryId = this.props.lottery.id
     this.setState({waiting: 1})
-    $.post('tailgate/User/Lottery', {
-      command: 'pickSpot',
-      spotId: spotId,
-      lotteryId: lotteryId
-    }, null, 'json').done(function (data) {
-      if (data.success) {
-        this.props.loadData()
-      } else {
-        this.setState({waiting: 0})
-        this.setState({
-          message: <div className="alert alert-danger">Your requested spot was chosen by someone else. Pick again.</div>
-        })
-        this.loadAvailableSpots()
-      }
-    }.bind(this))
+    $.post(
+      'tailgate/User/Lottery',
+      {
+        command: 'pickSpot',
+        spotId: spotId,
+        lotteryId: lotteryId,
+      },
+      null,
+      'json'
+    ).done(
+      function (data) {
+        if (data.success) {
+          this.props.loadData()
+        } else {
+          this.setState({waiting: 0})
+          this.setState({
+            message: (
+              <div className="alert alert-danger">
+                Your requested spot was chosen by someone else. Pick again.
+              </div>
+            ),
+          })
+          this.loadAvailableSpots()
+        }
+      }.bind(this)
+    )
   }
 
   updateSpot(e) {
-    this.setState({spotChoice : e.target.value})
+    this.setState({spotChoice: e.target.value})
   }
 
-  render () {
-    var sober
+  render() {
+    let sober
     if (this.state.waiting === 1) {
-      return <Waiting/>
+      return <Waiting />
     }
     if (this.state.availableSpots.length > 0) {
-      var options = this.state.availableSpots.map(function (val, i) {
+      const options = this.state.availableSpots.map(function (val, i) {
         if (val.sober === '1') {
           sober = '(Non-alcoholic)'
         } else {
           sober = ''
         }
         return (
-          <option key={i} value={val.id}>{val.lot_title + ', #' + val.number + ' ' + sober}</option>
+          <option key={i} value={val.id}>
+            {val.lot_title + ', #' + val.number + ' ' + sober}
+          </option>
         )
       })
       return (
         <div className="row">
           <div className="col-sm-12">
-            <p>You have confirmed your winning ticket. Pick the spot you wish to tailgate in
-              below. Choose quickly, other winners may be picking spots while you decide.</p>
+            <p>
+              You have confirmed your winning ticket. Pick the spot you wish to
+              tailgate in below. Choose quickly, other winners may be picking
+              spots while you decide.
+            </p>
             {this.state.message}
           </div>
           <div className="col-sm-4">
-            <select className="form-control" onChange={this.updateSpot} defaultValue="0">
-              <option disabled={true} value="0">- Choose below -</option>
+            <select
+              className="form-control"
+              onChange={this.updateSpot}
+              defaultValue="0">
+              <option disabled={true} value="0">
+                - Choose below -
+              </option>
               {options}
             </select>
           </div>
           <div className="col-sm-4">
-            <button className="btn btn-primary" onClick={this.confirmSpot}>Confirm my tailgating spot</button>
+            <button className="btn btn-primary" onClick={this.confirmSpot}>
+              Confirm my tailgating spot
+            </button>
           </div>
         </div>
       )
@@ -96,7 +126,7 @@ class ConfirmSpot extends React.Component {
 
 ConfirmSpot.propTypes = {
   lottery: PropTypes.object,
-  loadData: PropTypes.func
+  loadData: PropTypes.func,
 }
 
 export default ConfirmSpot
